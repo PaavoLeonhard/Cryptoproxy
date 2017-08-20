8bbeb4b0a230556388a982e837e05592a5b53925
Sebastian Werner <sebastian.werner@outlook.com>
2017-08-20 18:35:42 +0200

    refactoring & stremelined process, now works for any get request works with https forwoards stub for ident server

---
+++ src/main/java/de.tub.ise.cryptoware/App.java
@@ -0,0 +1,14 @@
+package de.tub.ise.cryptoware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *Start Spring-Boot server at 8080
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+	}
+}
---
+++ src/main/java/de.tub.ise.cryptoware/util/RestRequestService.java
@@ -0,0 +1,18 @@
+package de.tub.ise.cryptoware.util;
+
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class RestRequestService {
+
+    public final OkHttpClient client;
+
+    public RestRequestService(){
+        client = new OkHttpClient();
+    }
+
+
+}
