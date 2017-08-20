f969b1788dee50e619c060769770b77645c4bb01
Sebastian Werner <sebastian.werner@outlook.com>
2017-08-20 18:34:52 +0200

    refactoring & stremelined process, now works for any get request works with https forwoards stub for ident server

---
+++ src/main/java/de.tub.ise.cryptoware/data/Data.java
@@ -0,0 +1,22 @@
+package de.tub.ise.cryptoware.data;
+
+/**
+ * Wrapper class for a String since Spring-Boot sends data as a JSON
+ * @author Paavo.Camps
+ */
+public class Data {
+	private String data;
+
+	public Data(String data) {
+		this.data = data;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
---
+++ src/main/java/de.tub.ise.cryptoware/util/IdentService.java
@@ -0,0 +1,13 @@
+package de.tub.ise.cryptoware.util;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("prototype")
+public class IdentService {
+
+    public String getKeyFor(String identy,String server){
+        return "password1234funf";
+    }
+}
